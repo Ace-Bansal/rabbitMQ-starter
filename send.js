0a070b7bf454547dddc8ad55604739a361830989
Ace-Bansal <bansalekansh@gmail.com>
2019-12-31 13:01:54 +0530

    Add send.js file.

---
+++ send.js
@@ -0,0 +1,26 @@
+var amqp = require("amqplib/callback_api")
+
+amqp.connect("amqp://localhost", function(err, connection){
+    if(err){
+        throw err
+    }
+    connection.createChannel(function(err, channel){
+        if(err){
+            throw err
+        }
+        var queue = "hello"
+        var msg = "Hello world"
+
+        channel.assertQueue(queue, {
+            durable: false
+        })
+
+        channel.sendToQueue(queue, Buffer.from(msg))
+        console.log("Sent", msg)
+
+        setTimeout(function() { 
+            connection.close(); 
+            process.exit(0) 
+            }, 500);
+    })
+})
