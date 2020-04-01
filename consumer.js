e32ae011cb8e130f3522b865beeb001880b11678
Ace-Bansal <bansalekansh@gmail.com>
2020-04-01 15:39:20 +0530

    Add all files.

---
+++ consumer.js
@@ -0,0 +1,29 @@
+var amqp = require("amqplib/callback_api");
+
+amqp.connect("amqp://localhost", function (err, conn) {
+    if (err) {
+        throw err;
+    }
+    conn.createChannel(function (err, channel) {
+        if (err) {
+            throw err;
+        }
+        var queue = 'task_queue';
+
+        channel.assertQueue(queue, {
+            durable: true
+        });
+
+        channel.consume(queue, function (msg) {
+            var secs = msg.content.toString().split('.').length - 1;
+
+            console.log(" [x] Received %s", msg.content.toString());
+            setTimeout(function () {
+                console.log(" [x] Done");
+            }, secs * 1000);
+        }, {
+            noAck: false
+        });
+    })
+})
+
